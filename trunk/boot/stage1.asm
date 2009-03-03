4869921dc19d07aac496d6a46bf9481b23ca0a2f
nekros729 <nekros729@152bccaa-06ad-11de-bbfd-a7c1bc4c261e>
2009-03-03 02:02:18 +0000

    Added readsector function

--- trunk/boot/stage1.asm
+++ trunk/boot/stage1.asm
@@ -44,17 +44,65 @@ jmp testandset ;we are not done
 
 
 continue: ;here is where the rest of the code comes in
+hlt ; halt cpu for now
 
 FAIL:
 int 0x19 ;reboot
+
+
+;;;;;;;;;;FUNCTIONS;;;;;;;;;;;;;;
+readsector: ;ax = address to read to,   bx = lba
+push ax ;save address to  read to
+mov ax,WORD [SPT]
+mov cl,BYTE[HPC]
+mul cx ; HPC *SPT
+mov dx,ax
+mov ax,bx
+div dx  ;ax = LBA/ (HPC*SPT) dx = LBA % (HPC * SPT)
+mov BYTE [CYL],al ;save  cylinder (dx is now the same as TEMP)
+xor bx,bx  ;; bx = 0
+mov bl, BYTE [SPT] ; bx = SPT
+mov ax,dx
+div bx   ; TEMP / SPT
+mov BYTE [HEAD],al ; save head
+inc dx ; TEMP % SPT +1
+mov BYTE [SECTOR],dl
+pop ax ; restore ax to read address
+mov WORD [BUFFERPOINTER],ax ;save buffer
+startread:; HERE IS WEAR WE START THE READ
+xor bx,bx
+push bx
+pop es ; make sure es is 0
+mov bx,WORD [BUFFERPOINTER] ;set data buffer for int 13h
+mov dl, BYTE [drive]
+mov dh, BYTE [HEAD]
+mov cl, BYTE [SECTOR]
+mov ch, BYTE [CYL]
+mov al,1 ;always read one sector
+mov ah,0x02 ; int 0x13 read sector function
+cmp BYTE [counter],4
+je FAIL ;failed to read sectors
+int 0x13
+inc BYTE [counter]
+jc startread ; try again if failed the first 3 times
+
+;CYL = LBA / (HPC * SPT)
+;TEMP = LBA % (HPC * SPT)
+;HEAD = TEMP / SPT
+;SECT = TEMP % SPT + 1
 ;;;;;;;;;;;;;;;;;DATA AREA;;;;;;;;;;;;;;;;;;;
 stackend:
 times 10 dw 0 ; we only need a small stack
 stackstart:
-
+counter: db 0
 drive: db 0 ;this is where we store the drive we booted from
 HPC: db 0     ;heads per cylinder
 SPT: dw 0     ;sectors per track
+
+CYL: db 0     ;cylinder to read from
+HEAD: db 0 ;HEAD to read from
+SECTOR: db 0 ;sector to read  from
+BUFFERPOINTER: dw 0; address to read sectors to
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 times 510-($-$$) db 0 ;;make sure the file is 512 bytes
 dw 0xAA55;//magic number tells bios this is bootable
