b81a4342d42f647a1f5689fec75d0b9d75bc081b
nekros729 <nekros729@152bccaa-06ad-11de-bbfd-a7c1bc4c261e>
2009-03-04 02:49:54 +0000

    Fixed Halt Error

--- trunk/boot/stage1.asm
+++ trunk/boot/stage1.asm
@@ -1,35 +1,33 @@
 ;Copyright (C) Kristian Hart 2009
 ;Assemble with FASM
 USE16 ;this is all 16 bit code
+ORG 0x7c00
 ORG 0x7C00
 jmp 0x00:begin ;enforce address
 
 
 begin:
-mov sp,stackstart
-push cs; THANKS TROY!
-pop ds
-push cs
-pop es    ;done setting segments
+mov sp,(0x7c00-1)
+xor ax,ax
+mov ds,ax
+mov es,ax
 mov BYTE[drive],dl     ; store the boot drive
-;;;;;;;;;;;;;;;Load next 3 Sectors To complete stage1;;;;;;;;;;;;;;;;;;
-
 
 mov ah,0x08
-mov dl,BYTE [drive] 
-int 0x13        ; get drive parameters
-jc FAIL     ;did this faiL?
+mov dl,BYTE [drive]
+int 0x13 ; get drive parameters
+jc FAIL ;did this faiL?
 ;else store info for LBA TO CHS
-mov BYTE [HPC], dh ;//store heads per cylinder 
+mov BYTE [HPC], dh ;//store heads per cylinder
 
 mov ax,0 ;; start counter for all the bits
 
 testandset: ; tests and sets all the sector bits in cl
-bt cx,ax     ; test bit
-jc set       ; is the bit set? if yes set it in SPT
-inc ax    ; if no go to next bit and test set
+bt cx,ax ; test bit
+jc set ; is the bit set? if yes set it in SPT
+inc ax ; if no go to next bit and test set
 test ax,6
-je continue ;if we are finished, continue with the rest of  the code
+je continue ;if we are finished, continue with the rest of the code
 jmp testandset
 
 
@@ -40,30 +38,33 @@ test ax,6
 je continue ;if done setting, continue
 jmp testandset ;we are not done
 ;CL = maximum sector number (bits 5-0)
+
 continue:
 mov si,randstring
 call bios_print
-hlt ; halt cpu for now
+hang:
+jmp hang
+ ; halt cpu for now
 
 FAIL:
-mov si,randstring
+mov si,failone
 call bios_print
 hlt;
 
 ;;;;;;;;;;FUNCTIONS;;;;;;;;;;;;;;
-readsector: ;ax = address to read to,   bx = lba, on the stack is an 16 bit number specifying number of sectors to read
-push ax ;save address to  read to
+readsector: ;ax = address to read to, bx = lba, on the stack is an 16 bit number specifying number of sectors to read
+push ax ;save address to read to
 mov ax,WORD [SPT]
 mov cl,BYTE[HPC]
 mul cx ; HPC *SPT
 mov dx,ax
 mov ax,bx
-div dx  ;ax = LBA/ (HPC*SPT) dx = LBA % (HPC * SPT)
-mov BYTE [CYL],al ;save  cylinder (dx is now the same as TEMP)
-xor bx,bx  ;; bx = 0
+div dx ;ax = LBA/ (HPC*SPT) dx = LBA % (HPC * SPT)
+mov BYTE [CYL],al ;save cylinder (dx is now the same as TEMP)
+xor bx,bx ;; bx = 0
 mov bl, BYTE [SPT] ; bx = SPT
 mov ax,dx
-div bx   ; TEMP / SPT
+div bx ; TEMP / SPT
 mov BYTE [HEAD],al ; save head
 inc dx ; TEMP % SPT +1
 mov BYTE [SECTOR],dl
@@ -94,29 +95,32 @@ ret ;on succeed, return to main code
 
 
 bios_print: ;I was too lazy too actually right this so thanks babystep2...
-   lodsb
-   cmp al,0  ;zero=end of str
-   jz done    ;get out
-   mov ah, 0x0E
-   int 0x10
-   jmp bios_print
+lodsb
+or al,al ;zero=end of str
+jz done ;get out
+mov ah, 0x0E
+int 0x10
+jmp bios_print
 done:
-   ret
+ret
 ;;;;;;;;;;;;;;;;;DATA AREA;;;;;;;;;;;;;;;;;;;
 stackend:
 times 10 dw 0 ; we only need a small stack
 stackstart:
 counter: db 0
 drive: db 0 ;this is where we store the drive we booted from
-HPC: db 0     ;heads per cylinder
-SPT: dw 0     ;sectors per track
+HPC: db 0 ;heads per cylinder
+SPT: dw 0 ;sectors per track
 
-CYL: db 0     ;cylinder to read from
+CYL: db 0 ;cylinder to read from
 HEAD: db 0 ;HEAD to read from
-SECTOR: db 0 ;sector to read  from
+SECTOR: db 0 ;sector to read from
 BUFFERPOINTER: dw 0; address to read sectors to
 
 randstring: db 'I_WIN_BIZATCHES',0
+failone: db 'FAIL ONE',0
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 times 510-($-$$) db 0 ;;make sure the file is 512 bytes
 dw 0xAA55;magic number tells bios this is bootable
+
+
